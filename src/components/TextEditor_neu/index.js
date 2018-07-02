f533580766d528ed7af4a2e8994548d81b1f2090
Ferhat Topcu <topcu.ft@gmail.com>
2018-07-03 00:01:28 +0200

    reformat init_text

--- src/components/TextEditor_neu/index.js
+++ src/components/TextEditor_neu/index.js
@@ -26,7 +26,31 @@ const boxTarget = {
   }
 }
 
-const init_text = '<h1>Erde</h1><p>Die <b>Erde</b> ist der <a href="/wiki/Dichte" title="Dichte">dichteste</a>, fünftgrößte und der <a href="/wiki/Sonne" title="Sonne">Sonne</a> drittnächste <a href="/wiki/Planet" title="Planet">Planet</a> des <a href="/wiki/Sonnensystem" title="Sonnensystem">Sonnensystems</a>. Sie ist Ursprungsort und Heimat aller bekannten <a href="/wiki/Lebewesen" title="Lebewesen">Lebewesen</a>. Ihr Durchmesser beträgt mehr als        <a href="/wiki/Kilometer" class="mw-redirect" title="Kilometer">Kilometer</a> und ihr Alter etwa 4,6 Milliarden <a href="/wiki/Jahr" title="Jahr">Jahre</a>. Nach ihrer vorherrschenden <a href="/wiki/Geochemie" title="Geochemie">geochemischen</a> Beschaffenheit wurde der Begriff der „<a href="/wiki/Erd%C3%A4hnlicher_Planet" class="mw-redirect" title="Erdähnlicher Planet">erdähnlichen Planeten</a>“ geprägt. Das <a href="/wiki/Astronomisches_Symbol" title="Astronomisches Symbol">astronomische Symbol</a> der Erde ist <a href="/wiki/%E2%99%81" title="♁">♁</a> oder <a href="/wiki/Radkreuz" title="Radkreuz">⊕</a>.<sup id="cite_ref-4" class="reference"><a href="#cite_note-4">[4]</a></sup></p><p>Da die Erdoberfläche zu etwa zwei Dritteln aus <a href="/wiki/Wasser" title="Wasser">Wasser</a> besteht und daher die Erde vom <a href="/wiki/Weltraum" title="Weltraum">All</a> betrachtet vorwiegend blau erscheint, wird sie auch <b>Blauer Planet</b> genannt. Sie wird <a href="/wiki/Metapher" title="Metapher">metaphorisch</a> auch als „<a href="/wiki/Raumschiff_Erde" title="Raumschiff Erde">Raumschiff Erde</a>“ bezeichnet.</p><p>Das Wort für Erde ist in fast allen Sprachen <a href="/wiki/Femininum" class="mw-redirect" title="Femininum">femininum</a>.<sup id="cite_ref-5" class="reference"><a href="#cite_note-5">[5]</a></sup> Die Erde spielt als Lebensgrundlage des Menschen in allen <a href="/wiki/Religion" title="Religion">Religionen</a> eine herausragende Rolle als <a href="/wiki/Heilig" title="Heilig">heilige</a> <a href="/wiki/Ganzheit" title="Ganzheit">Ganzheit</a>; in etlichen <a href="/wiki/Ethnische_Religionen" title="Ethnische Religionen">ethnischen-</a>, <a href="/wiki/Volksfr%C3%B6mmigkeit" title="Volksfrömmigkeit">Volks-</a> und <a href="/wiki/Historische_Religion" class="mw-redirect" title="Historische Religion">historischen Religionen</a> entweder als diffuse <a href="/wiki/Gott" title="Gott">Vergöttlichung</a> einer „<a href="/wiki/Mutter_Erde" title="Mutter Erde">Mutter Erde</a>“ oder als personifizierte <a href="/wiki/Erdg%C3%B6ttin" title="Erdgöttin">Erdgöttin</a>.<sup id="cite_ref-6" class="reference"><a href="#cite_note-6">[6]</a></sup></p>'
+const init_text = `
+  <h1>Erde</h1>
+  <p>Die <b>Erde</b> ist der <a href="/wiki/Dichte" title="Dichte">dichteste</a>,
+  fünftgrößte und der <a href="/wiki/Sonne" title="Sonne">Sonne</a> drittnächste
+  <a href="/wiki/Planet" title="Planet">Planet</a> des <a href="/wiki/Sonnensystem" title="Sonnensystem">Sonnensystems</a>.
+  Sie ist Ursprungsort und Heimat aller bekannten <a href="/wiki/Lebewesen" title="Lebewesen">Lebewesen</a>. 
+  Ihr Durchmesser beträgt mehr als        <a href="/wiki/Kilometer" class="mw-redirect" title="Kilometer">Kilometer</a> 
+  und ihr Alter etwa 4,6 Milliarden <a href="/wiki/Jahr" title="Jahr">Jahre</a>. Nach ihrer 
+  vorherrschenden <a href="/wiki/Geochemie" title="Geochemie">geochemischen</a> 
+  Beschaffenheit wurde der Begriff der „<a href="/wiki/Erd%C3%A4hnlicher_Planet" class="mw-redirect" title="Erdähnlicher Planet">erdähnlichen Planeten</a>“ 
+  geprägt. Das <a href="/wiki/Astronomisches_Symbol" title="Astronomisches Symbol">astronomische Symbol</a> 
+  der Erde ist <a href="/wiki/%E2%99%81" title="♁">♁</a> oder <a href="/wiki/Radkreuz" title="Radkreuz">⊕</a>.
+  <sup id="cite_ref-4" class="reference"><a href="#cite_note-4">[4]</a></sup></p>
+  <p>Da die Erdoberfläche zu etwa zwei Dritteln aus <a href="/wiki/Wasser" title="Wasser">Wasser</a> 
+  besteht und daher die Erde vom <a href="/wiki/Weltraum" title="Weltraum">All</a> betrachtet vorwiegend blau erscheint, 
+  wird sie auch <b>Blauer Planet</b> genannt. Sie wird <a href="/wiki/Metapher" title="Metapher">metaphorisch</a> 
+  auch als „<a href="/wiki/Raumschiff_Erde" title="Raumschiff Erde">Raumschiff Erde</a>“ bezeichnet.</p>
+  <p>Das Wort für Erde ist in fast allen Sprachen <a href="/wiki/Femininum" class="mw-redirect" title="Femininum">femininum</a>.
+  <sup id="cite_ref-5" class="reference"><a href="#cite_note-5">[5]</a></sup> 
+  Die Erde spielt als Lebensgrundlage des Menschen in allen <a href="/wiki/Religion" title="Religion">Religionen</a> 
+  eine herausragende Rolle als <a href="/wiki/Heilig" title="Heilig">heilige</a> <a href="/wiki/Ganzheit" title="Ganzheit">Ganzheit</a>; 
+  in etlichen <a href="/wiki/Ethnische_Religionen" title="Ethnische Religionen">ethnischen-</a>, <a href="/wiki/Volksfr%C3%B6mmigkeit" title="Volksfrömmigkeit">Volks-</a> 
+  und <a href="/wiki/Historische_Religion" class="mw-redirect" title="Historische Religion">historischen Religionen</a> 
+  entweder als diffuse <a href="/wiki/Gott" title="Gott">Vergöttlichung</a> einer „<a href="/wiki/Mutter_Erde" title="Mutter Erde">Mutter Erde</a>“ 
+  oder als personifizierte <a href="/wiki/Erdg%C3%B6ttin" title="Erdgöttin">Erdgöttin</a>.<sup id="cite_ref-6" class="reference"><a href="#cite_note-6">[6]</a></sup></p>`
 
 class TextEditor_neu extends React.Component {
   state = {
